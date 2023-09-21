ea51bf1f6802d80d7edeefba33c0e1179916b812
Emile Tenezakis <e.tenezakis@gmail.com>
2023-09-21 17:21:46 +0900

    Fix/config validators (#16)
    * fix config validators by using model validator
    
    * more config fixes, add package json file for CDK CLI version
    
    * change defaults

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ Demonstration application showing the use and configuration options of the [eoap
 
 - python
 - docker
-- the AWS CDK CLI
+- node
 - AWS credentials environment variables configured to point to an account. 
 - **Optional** a `config.yaml` file to override the default deployment settings defined in `config.py`.
 
 ## Installation
+
+Install python dependencies with 
 
 ```
 python -m venv .venv
@@ -18,16 +20,27 @@ source .venv/bin/activate
 python -m pip install -r requirements.txt
 ```
 
+And node dependencies with 
+
+```
+npm install
+```
+
+Verify that the `cdk` CLI is available. Since `aws-cdk` is installed as a local dependency, you can use the `npx` node package runner tool, that comes with `npm`.
+
+```
+npx cdk --version
+```
 ## Deployment
 
 First, synthesize the app 
 
 ```
-cdk synth --all
+npx cdk synth --all
 ```
 
 Then, deploy
 
 ```
-cdk deploy --all --require-approval never
+npx cdk deploy --all --require-approval never
 ```
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Union
 
 import pydantic
 import yaml
@@ -6,32 +6,27 @@ from aws_cdk import aws_ec2
 from pydantic_core.core_schema import FieldValidationInfo
 from pydantic_settings import BaseSettings
 
-DEFAULT_PROJECT_ID = "eoapi-template-demo"
-DEFAULT_STAGE = "test"
-DEFAULT_NAT_GATEWAY_COUNT = 1
-
 
 class AppConfig(BaseSettings):
-    project_id: Optional[str] = pydantic.Field(
-        description="Project ID", default=DEFAULT_PROJECT_ID
+    project_id: str = pydantic.Field(
+        description="Project ID", default="eoapi-template-demo"
     )
-    stage: Optional[str] = pydantic.Field(
-        description="Stage of deployment", default=DEFAULT_STAGE
-    )
-    tags: Optional[Dict[str, str]] = pydantic.Field(
+    stage: str = pydantic.Field(description="Stage of deployment", default="test")
+    # because of its validator, `tags` should always come after `project_id` and `stage`
+    tags: Dict[str, str] | None = pydantic.Field(
         description="""Tags to apply to resources. If none provided, 
         will default to the defaults defined in `default_tags`.
         Note that if tags are passed to the CDK CLI via `--tags`, 
         they will override any tags defined here.""",
         default=None,
     )
-    auth_provider_jwks_url: Optional[str] = pydantic.Field(
+    auth_provider_jwks_url: str | None = pydantic.Field(
         description="""Auth Provider JSON Web Key Set URL for
         ingestion authentication. If not provided, 
         no authentication will be required.""",
         default=None,
     )
-    data_access_role_arn: Optional[str] = pydantic.Field(
+    data_access_role_arn: str | None = pydantic.Field(
         description="""Role ARN for data access, that will be
         used by the STAC ingestor for validation of assets
         located in S3 and for the tiler application to access
@@ -42,72 +37,73 @@ class AppConfig(BaseSettings):
         assume it. See https://github.com/developmentseed/eoapi-cdk""",
         default=None,
     )
-    db_instance_type: Optional[str] = pydantic.Field(
+    db_instance_type: str = pydantic.Field(
         description="Database instance type", default="t3.micro"
     )
-    db_allocated_storage: Optional[int] = pydantic.Field(
+    db_allocated_storage: int = pydantic.Field(
         description="Allocated storage for the database", default=5
     )
-    public_db_subnet: Optional[bool] = pydantic.Field(
-        description="Whether to put the database in a public subnet", default=False
+    public_db_subnet: bool = pydantic.Field(
+        description="Whether to put the database in a public subnet", default=True
     )
-    nat_gateway_count: Optional[int] = pydantic.Field(
+    nat_gateway_count: int = pydantic.Field(
         description="Number of NAT gateways to create",
-        default=DEFAULT_NAT_GATEWAY_COUNT,
+        default=0,
     )
-    bastion_host: Optional[bool] = pydantic.Field(
+    bastion_host: bool = pydantic.Field(
         description="""Whether to create a bastion host. It can typically 
         be used to make administrative connections to the database if 
         `public_db_subnet` is False""",
-        default=True,
-    )
-    bastion_host_create_elastic_ip: Optional[bool] = pydantic.Field(
-        description="Whether to create an elastic IP for the bastion host",
         default=False,
     )
-    bastion_host_allow_ip_list: Optional[List[str]] = pydantic.Field(
+    bastion_host_create_elastic_ip: bool = pydantic.Field(
+        description="""Whether to create an elastic IP for the bastion host.
+        Ignored if `bastion_host` equals `False`""",
+        default=False,
+    )
+    bastion_host_allow_ip_list: List[str] = pydantic.Field(
         description="""YAML file containing list of IP addresses to 
-        allow SSH access to the bastion host""",
+        allow SSH access to the bastion host. Ignored if `bastion_host`
+        equals `False`.""",
         default=[],
     )
-    bastion_host_user_data: Optional[
-        Union[Dict[str, Any], aws_ec2.UserData]
-    ] = pydantic.Field(
-        description="Path to file containing user data for the bastion host",
+    bastion_host_user_data: Union[Dict[str, Any], aws_ec2.UserData] = pydantic.Field(
+        description="""Path to file containing user data for the bastion host.
+        Ignored if `bastion_host` equals `False`.""",
         default=aws_ec2.UserData.for_linux(),
     )
-    titiler_buckets: Optional[List[str]] = pydantic.Field(
+    titiler_buckets: List[str] = pydantic.Field(
         description="""Path to YAML file containing list of
         buckets to grant access to the titiler API""",
         default=[],
     )
-    acm_certificate_arn: Optional[str] = pydantic.Field(
+    acm_certificate_arn: str | None = pydantic.Field(
         description="""ARN of ACM certificate to use for 
         custom domain names. If provided,
         CDNs are created for all the APIs""",
         default=None,
     )
-    stac_api_custom_domain: Optional[str] = pydantic.Field(
+    stac_api_custom_domain: str | None = pydantic.Field(
         description="""Custom domain name for the STAC API. 
         Must provide `acm_certificate_arn`""",
         default=None,
     )
-    titiler_pgstac_api_custom_domain: Optional[str] = pydantic.Field(
+    titiler_pgstac_api_custom_domain: str | None = pydantic.Field(
         description="""Custom domain name for the titiler pgstac API. 
         Must provide `acm_certificate_arn`""",
         default=None,
     )
-    stac_ingestor_api_custom_domain: Optional[str] = pydantic.Field(
+    stac_ingestor_api_custom_domain: str | None = pydantic.Field(
         description="""Custom domain name for the STAC ingestor API.
         Must provide `acm_certificate_arn`""",
         default=None,
     )
-    tipg_api_custom_domain: Optional[str] = pydantic.Field(
+    tipg_api_custom_domain: str | None = pydantic.Field(
         description="""Custom domain name for the tipg API. 
         Must provide `acm_certificate_arn`""",
         default=None,
     )
-    stac_browser_version: Optional[str] = pydantic.Field(
+    stac_browser_version: str | None = pydantic.Field(
         description="""Version of the Radiant Earth STAC browser to deploy.
         If none provided, no STAC browser will be deployed.
         If provided, `stac_api_custom_domain` must be provided
@@ -119,9 +115,11 @@ class AppConfig(BaseSettings):
     def default_tags(cls, v, info: FieldValidationInfo):
         return v or {"project_id": info.data["project_id"], "stage": info.data["stage"]}
 
-    @pydantic.field_validator("nat_gateway_count")
-    def validate_nat_gateway_count(cls, v, info: FieldValidationInfo):
-        if not info.data["public_db_subnet"] and v <= 0:
+    @pydantic.model_validator(mode="after")
+    def validate_nat_gateway_count(self) -> "AppConfig":
+        if not self.public_db_subnet and (
+            self.nat_gateway_count is not None and self.nat_gateway_count <= 0
+        ):
             raise ValueError(
                 """if the database and its associated services instances
                              are to be located in the private subnet of the VPC, NAT
@@ -129,26 +127,29 @@ class AppConfig(BaseSettings):
                              and therefore `nat_gateway_count` has to be > 0."""
             )
         else:
-            return v
+            return self
 
-    @pydantic.field_validator("stac_browser_version")
-    def validate_stac_browser_version(cls, v, info: FieldValidationInfo):
-        if v is not None and info.data["stac_api_custom_domain"] is None:
+    @pydantic.model_validator(mode="after")
+    def validate_stac_browser_version(self) -> "AppConfig":
+        if (
+            self.stac_browser_version is not None
+            and self.stac_api_custom_domain is None
+        ):
             raise ValueError(
                 """If a STAC browser version is provided, 
                 a custom domain must be provided for the STAC API"""
             )
         else:
-            return v
+            return self
 
-    @pydantic.field_validator("acm_certificate_arn")
-    def validate_acm_certificate_arn(cls, v, info: FieldValidationInfo):
-        if v is None and any(
+    @pydantic.model_validator(mode="after")
+    def validate_acm_certificate_arn(self) -> "AppConfig":
+        if self.acm_certificate_arn is None and any(
             [
-                info.data["stac_api_custom_domain"],
-                info.data["titiler_pgstac_api_custom_domain"],
-                info.data["stac_ingestor_api_custom_domain"],
-                info.data["tipg_api_custom_domain"],
+                self.stac_api_custom_domain,
+                self.titiler_pgstac_api_custom_domain,
+                self.stac_ingestor_api_custom_domain,
+                self.tipg_api_custom_domain,
             ]
         ):
             raise ValueError(
@@ -156,7 +157,7 @@ class AppConfig(BaseSettings):
                 an ACM certificate ARN must be provided"""
             )
         else:
-            return v
+            return self
 
     def build_service_name(self, service_id: str) -> str:
         return f"{self.project_id}-{self.stage}-{service_id}"
